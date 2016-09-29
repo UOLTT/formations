abbeba0ed81257e4c1acff8c5d91cb055ac88f7f
judahnator <judah@thosetechguys.org>
2016-09-29 12:26:51 -0500

    adding a manifesto section to the org page

--- resources/views/organizations/show.blade.php
+++ resources/views/organizations/show.blade.php
@@ -7,30 +7,36 @@
     </div>
 
     <div class="row">
-        <table class="table table-striped">
-            <tbody>
-            <tr>
-                <td><b>Organization Manager:</b></td>
-                <td>{{ $Organization->administrator->name }}</td>
-            </tr>
-            <tr>
-                <td><b>Management URL:</b></td>
-                <td>{{ env('APP_URL').'/'.$Organization->domain.'/' }}</td>
-            </tr>
-            <tr>
-                <td><b>Users:</b></td>
-                <td>{{ $Organization->users_count }}</td>
-            </tr>
-            <tr>
-                <td><b>Fleets:</b></td>
-                <td>{{ $Organization->fleets_count }}</td>
-            </tr>
-            <tr>
-                <td><b>Squadrons:</b></td>
-                <td>{{ $Organization->squads_count }}</td>
-            </tr>
-            </tbody>
-        </table>
+        <div class="col-md-6">
+            <table class="table table-striped">
+                <tbody>
+                <tr>
+                    <td><b>Organization Manager:</b></td>
+                    <td>{{ $Organization->administrator->name }}</td>
+                </tr>
+                <tr>
+                    <td><b>Management URL:</b></td>
+                    <td>{{ env('APP_URL').'/'.$Organization->domain.'/' }}</td>
+                </tr>
+                <tr>
+                    <td><b>Users:</b></td>
+                    <td>{{ $Organization->users_count }}</td>
+                </tr>
+                <tr>
+                    <td><b>Fleets:</b></td>
+                    <td>{{ $Organization->fleets_count }}</td>
+                </tr>
+                <tr>
+                    <td><b>Squadrons:</b></td>
+                    <td>{{ $Organization->squads_count }}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-6">
+            <h4>Manifesto:</h4>
+            <p>{{ str_replace("\r\n","<br><br>",$Organization->manifesto) }}</p>
+        </div>
     </div>
 
 @endsection
