8b5e184d4d49b56a67f67ea28d0bb01123400cf2
judahnator <judah@thosetechguys.org>
2016-10-17 16:35:56 -0500

    fixing casts for ships

--- app/Ship.php
+++ app/Ship.php
@@ -9,6 +9,31 @@ class Ship extends Model
 
     public $timestamps = false;
     protected $table = 'ships';
+    protected $casts = [
+        'id' => 'integer',
+        'shipname' => 'string',
+        'length' => 'integer',
+        'beam' => 'integer',
+        'height' => 'integer',
+        'nullmass' => 'integer',
+        'cargocapacity' => 'integer',
+        'crew' => 'integer',
+        'powerplant' => 'integer',
+        'powercount' => 'integer',
+        'primary' => 'integer',
+        'pcount' => 'integer',
+        'maneuvering' => 'integer',
+        'mancount' => 'integer',
+        'shield' => 'integer',
+        'shieldcount' => 'integer',
+        'raceenabled' => 'string',
+        'price' => 'integer',
+        'class' => 'string',
+        'combatspeed' => 'integer',
+        'combatrating' => 'integer',
+        'waverank' => 'integer'
+    ];
+
 
     public function users() {
         return $this->belongsToMany(User::class,'ship_user');
