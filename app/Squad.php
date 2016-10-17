b3288a75d2adcbb12ab44cc66f0229b21a3035fe
judahnator <judah@thosetechguys.org>
2016-10-17 14:07:50 -0500

    adding casts to squad model

--- app/Squad.php
+++ app/Squad.php
@@ -6,10 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Squad extends Model
 {
-
+    // TODO maybe add fleet admin?
     public $timestamps = true;
     protected $table = 'squads';
     protected $fillable = ['fleet_id','name','status_id'];
+    protected $casts = [
+        'id' => 'integer',
+        'fleet_id' => 'integer',
+        'status_id' => 'integer',
+        'name' => 'string'
+    ];
 
     public function fleet() {
         return $this->belongsTo(Fleet::class);
