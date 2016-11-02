4676ec7b804a357290f9473275ae143b65dec235
judahnator <judah@thosetechguys.org>
2016-11-02 14:34:01 -0500

    adding positions model and migration

---
+++ app/Position.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Position extends Model
+{
+    public $timestamps = false;
+    protected $table = 'positions';
+    protected $fillable = ['formation_id','description','x-offset','y-offset','z-offset'];
+    protected $casts = [
+        'id' => 'integer',
+        'formation_id' => 'integer',
+        'description' => 'string',
+        'x-offset' => 'float',
+        'y-offset' => 'float',
+        'z-offset' => 'float'
+    ];
+}
