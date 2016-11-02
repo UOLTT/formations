bef6f7f746e4c23175223e044e329fd0688a9c82
judahnator <judah@thosetechguys.org>
2016-11-02 14:25:44 -0500

    filling out formation model

--- app/Formation.php
+++ app/Formation.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Formation extends Model
 {
-    //
+    public $timestamps = true;
+    protected $table = 'formations';
+    protected $hidden = ['created_at','updated_at'];
+    protected $fillable = ['name','description','minimum_users'];
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'description' => 'string',
+        'minimum_users' => 'integer'
+    ];
 }
