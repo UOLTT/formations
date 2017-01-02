dd81f0b65e3bfd43b4a317aa20d776fa26218db2
judahnator <judah@thosetechguys.org>
2017-01-02 10:52:42 -0600

    Adding filename as an option in the formations table

--- app/Formation.php
+++ app/Formation.php
@@ -9,7 +9,7 @@ class Formation extends Model
     public $timestamps = true;
     protected $table = 'formations';
     protected $hidden = ['created_at','updated_at'];
-    protected $fillable = ['name','description','minimum_users'];
+    protected $fillable = ['name','description','minimum_users','filename'];
     protected $casts = [
         'id' => 'integer',
         'name' => 'string',
