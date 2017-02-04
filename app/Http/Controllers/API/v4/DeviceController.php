1896755c9f3571c357532f3199e03e2d7398823d
Judah Wright <judah@servercanyon.com>
2017-02-04 13:40:09 -0600

    Optimizing DeviceController

--- app/Http/Controllers/API/v4/DeviceController.php
+++ app/Http/Controllers/API/v4/DeviceController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API\v4;
 use App\Device;
 use App\Http\Controllers\API\ApiController;
 use App\User;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class DeviceController extends ApiController
@@ -14,12 +13,7 @@ class DeviceController extends ApiController
         $Device = Device::with('user')
             ->where('token',$token)
             ->where('used','=',false)
-            ->first();
-        if (is_null($Device)) {
-            return response([
-                'error' => "No query results for model [App\\Fleet] ".$token,
-                'status_code' => 404
-            ],404);        }
+            ->firstOrFail();
         $Device->used = true;
         $Device->save();
         return User::with('devices')
